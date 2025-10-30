9df64175f217bc7fc50d9a054af1610dfb849be2
vullioud <colino.vullioud@gmail.com>
2025-10-30 11:53:11 +0100

    clean

--- soco_src/utils/helpers.js
+++ soco_src/utils/helpers.js
@@ -1,7 +1,12 @@
-
-// Define a simple object literal. This creates the 'helpers' variable.
 var helpers = {
+    calculate_std_dev: function(arr) {
+        if (!arr || arr.length < 2) {
+            return 0;
+        }
+        const n = arr.length;
+        const mean = arr.reduce((a, b) => a + b) / n;
+        const variance = arr.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n;
+        return Math.sqrt(variance);
+    }
 };
-
-// EXPLICITLY ATTACH THE VARIABLE TO THE GLOBAL SCOPE. This will now work.
 this.helpers = helpers;
