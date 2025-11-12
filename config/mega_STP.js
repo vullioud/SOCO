81effb58d7790b063379bd92c6c9c6448892c4c5
vullioud <colino.vullioud@gmail.com>
2025-11-12 21:40:02 +0100

    New working structure through signal

--- config/mega_STP.js
+++ config/mega_STP.js
@@ -19,200 +19,59 @@ if (typeof lib === 'undefined') {
 
 console.log("--- Defining the SoCoABE Mega-STP (Full Version) ---");
 
-// --- I. DEFAULTS & HELPER ---
-
-const MEGA_STP_DEFAULTS = {
-    clearcut:           { rotation_age: 100 },
-    AllBigTrees:        { dbh_threshold: 60 },
-    shelterwood:        { times: 3, interval: 8, nTrees: 50, speciesSelectivity: {} },
-    femel:              { steps: 3, interval: 10, N: 4 },
-    targetDBH:          { times: 5, targetDBH: 50, dbhList: {} },
-    fromBelow:          { times: 4, interval: 8, thinningShare: 0.15, speciesSelectivity: {} },
-    selectiveThinning:  { times: 5, interval: 5, nTrees: 80, nCompetitors: 4, speciesSelectivity: {} },
-    plenter:            { interval: 10, plenterCurve: {} },
-    tending:            { times: 3, interval: 3, intensity: 10, speciesSelectivity: {} },
-    planting:           { speciesSelectivity: {} }
-};
-
+// --- Helper function to read flags with a default value ---
 function getFlag(flagName, defaultValue) {
+    // This function is defined globally within the STP's context.
     const value = stand.flag(flagName);
     return (value !== undefined && value !== null) ? value : defaultValue;
 }
 
 const MEGA_STP_ACTIVITIES = {};
 
-// --- II. ACTIVITY DEFINITIONS ---
+// --- ACTIVITY DEFINITIONS ---
 
 // 1. No Management
-MEGA_STP_ACTIVITIES['noManagement'] = lib.harvest.noManagement();
-MEGA_STP_ACTIVITIES['noManagement'].onEvaluate = function() {
-    return getFlag('abe_next_activity', 'none') === 'noManagement';
+MEGA_STP_ACTIVITIES['noManagement'] = {
+    id: 'MegaSTP_NoManagement',
+    type: 'general', // Use 'general' for simple, non-scheduled actions
+    schedule: { signal: 'do_noManagement' },
+    action: function() {
+        console.log(`[MEGA-STP] Executing 'noManagement' for stand ${stand.id}.`);
+        // This activity does nothing but sends a completion signal.
+        stand.stp.signal('Activity_Completed');
+    }
 };
 
 // 2. Clearcut
-MEGA_STP_ACTIVITIES['clearcut'] = lib.harvest.clearcut({
+MEGA_STP_ACTIVITIES['clearcut'] = {
     id: 'MegaSTP_Clearcut',
-    schedule: { opt: () => getFlag('abe_param_rotation_age', MEGA_STP_DEFAULTS.clearcut.rotation_age), force: true },
-    sendSignal: 'Activity_Completed'
-});
-MEGA_STP_ACTIVITIES['clearcut'].onEvaluate = function() {
-    return getFlag('abe_next_activity', 'none') === 'clearcut';
+    type: 'scheduled', // Use 'scheduled' because it's a harvest activity that should be logged correctly.
+    schedule: { signal: 'do_clearcut' },
+    finalHarvest: true, // This is a crucial flag for ABE to reset the stand's rotation age.
+
+    // onExecute is the correct event for a 'scheduled' activity's main logic.
+    onExecute: function() {
+        console.log(`[MEGA-STP] Executing 'clearcut' for stand ${stand.id}.`);
+
+        // Read the parameter from the flag at the moment of execution.
+        var preferenceFunction = getFlag('abe_param_preferenceFunction', 'dbh > 0');
+
+        // The 'simulate' flag is automatically handled by ABE for scheduled activities.
+        // We can just call the harvest logic.
+        stand.trees.load(preferenceFunction);
+        var harvested_count = stand.trees.harvest();
+        stand.trees.removeMarkedTrees(); // Ensure simulated harvests are executed.
+        console.log(`[MEGA-STP] -> Harvested ${harvested_count} trees.`);
+    },
+
+    // onExit is called after execution and is the correct place to send the completion signal.
+    onExit: function() {
+        stand.stp.signal('Activity_Completed');
+    }
 };
 
-// 3. Harvest All Big Trees
-MEGA_STP_ACTIVITIES['AllBigTrees'] = lib.harvest.HarvestAllBigTrees({
-    id: 'MegaSTP_AllBigTrees',
-    schedule: { min: 1, opt: 10, max: 500 },
-    preferenceFunction: () => 'dbh > ' + getFlag('abe_param_dbh_threshold', MEGA_STP_DEFAULTS.AllBigTrees.dbh_threshold),
-    sendSignal: 'Activity_Completed'
-});
-MEGA_STP_ACTIVITIES['AllBigTrees'].onEvaluate = function() {
-    return getFlag('abe_next_activity', 'none') === 'AllBigTrees';
-};
 
-// 4. Shelterwood
-// var shelterwoodProgram = lib.harvest.shelterwood({
-//     id: 'MegaSTP_Shelterwood',
-//     schedule: { min: 1, opt: 10, max: 500 },
-//     times:              () => getFlag('abe_param_times', MEGA_STP_DEFAULTS.shelterwood.times),
-//     interval:           () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.shelterwood.interval),
-//     nTrees:             () => getFlag('abe_param_nTrees', MEGA_STP_DEFAULTS.shelterwood.nTrees),
-//     speciesSelectivity: () => getFlag('abe_param_speciesSelectivity', MEGA_STP_DEFAULTS.shelterwood.speciesSelectivity),
-//     sendSignal: 'Activity_Completed'
-// });
-// for (var key in shelterwoodProgram) {
-//     if (typeof shelterwoodProgram[key] === 'object' && shelterwoodProgram[key].type) {
-//         shelterwoodProgram[key].onEvaluate = function() {
-//             return getFlag('abe_next_activity', 'none') === 'shelterwood';
-//         };
-//         MEGA_STP_ACTIVITIES['shelterwood_' + key] = shelterwoodProgram[key];
-//     }
-// }
-//
-// var femelProgram = {
-//     SelectPatches: lib.selectOptimalPatches({
-//         id: 'MegaSTP_Femel_Select',
-//         schedule: { min: 1, opt: 10, max: 500 },
-//         N: () => getFlag('abe_param_N', MEGA_STP_DEFAULTS.femel.N),
-//         patchsize: 2,
-//         criterium: 'max_basalarea',
-//         sendSignal: 'Internal_Femel_Patches_Selected'
-//     }),
-//     HarvestSequence: lib.harvest.femel({
-//         id: 'MegaSTP_Femel_Harvest',
-//         schedule: { signal: 'Internal_Femel_Patches_Selected' },
-//         steps:    () => getFlag('abe_param_steps', MEGA_STP_DEFAULTS.femel.steps),
-//         interval: () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.femel.interval),
-//         sendSignal: 'Activity_Completed'
-//     })
-// };
-// femelProgram.SelectPatches.onEvaluate = function() {
-//     return getFlag('abe_next_activity', 'none') === 'femel';
-// };
-// MEGA_STP_ACTIVITIES['femel_SelectPatches'] = femelProgram.SelectPatches;
-// for (var femelKey in femelProgram.HarvestSequence) {
-//     if (typeof femelProgram.HarvestSequence[femelKey] === 'object' && femelProgram.HarvestSequence[femelKey].type) {
-//         MEGA_STP_ACTIVITIES['femel_' + femelKey] = femelProgram.HarvestSequence[femelKey];
-//     }
-// }
-//
-// MEGA_STP_ACTIVITIES['targetDBH'] = lib.harvest.targetDBH({
-//     id: 'MegaSTP_TargetDBH',
-//     schedule: { repeat: true, repeatInterval: () => getFlag('abe_param_times', MEGA_STP_DEFAULTS.targetDBH.times) },
-//     targetDBH: () => getFlag('abe_param_targetDBH', MEGA_STP_DEFAULTS.targetDBH.targetDBH),
-//     dbhList:   () => getFlag('abe_param_dbhList', MEGA_STP_DEFAULTS.targetDBH.dbhList)
-// });
-// MEGA_STP_ACTIVITIES['targetDBH'].onEvaluate = function() {
-//     return getFlag('abe_next_activity', 'none') === 'targetDBH';
-// };
-//
-// var fromBelowProgram = lib.thinning.fromBelow({
-//     id: 'MegaSTP_FromBelow',
-//     schedule: { min: 20, opt: 30, max: 500 },
-//     thinningShare:      () => getFlag('abe_param_thinningShare', MEGA_STP_DEFAULTS.fromBelow.thinningShare),
-//     speciesSelectivity: () => getFlag('abe_param_speciesSelectivity', MEGA_STP_DEFAULTS.fromBelow.speciesSelectivity),
-//     times:              () => getFlag('abe_param_times', MEGA_STP_DEFAULTS.fromBelow.times),
-//     interval:           () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.fromBelow.interval),
-//     sendSignal: 'Activity_Completed'
-// });
-// for (var fbKey in fromBelowProgram) {
-//     if (typeof fromBelowProgram[fbKey] === 'object' && fromBelowProgram[fbKey].type) {
-//         fromBelowProgram[fbKey].onEvaluate = function() {
-//             return getFlag('abe_next_activity', 'none') === 'fromBelow';
-//         };
-//         MEGA_STP_ACTIVITIES['fromBelow_' + fbKey] = fromBelowProgram[fbKey];
-//     }
-// }
-//
-// var selectiveProgram = lib.thinning.selectiveThinning({
-//     id: 'MegaSTP_Selective',
-//     schedule: { min: 1, opt: 10, max: 500 },
-//     nTrees:             () => getFlag('abe_param_nTrees', MEGA_STP_DEFAULTS.selectiveThinning.nTrees),
-//     nCompetitors:       () => getFlag('abe_param_nCompetitors', MEGA_STP_DEFAULTS.selectiveThinning.nCompetitors),
-//     times:              () => getFlag('abe_param_times', MEGA_STP_DEFAULTS.selectiveThinning.times),
-//     interval:           () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.selectiveThinning.interval),
-//     speciesSelectivity: () => getFlag('abe_param_speciesSelectivity', MEGA_STP_DEFAULTS.selectiveThinning.speciesSelectivity),
-//     sendSignal: 'Activity_Completed'
-// });
-// for (var selKey in selectiveProgram) {
-//     if (typeof selectiveProgram[selKey] === 'object' && selectiveProgram[selKey].type) {
-//         selectiveProgram[selKey].onEvaluate = function() {
-//             return getFlag('abe_next_activity', 'none') === 'selectiveThinning';
-//         };
-//         MEGA_STP_ACTIVITIES['selectiveThinning_' + selKey] = selectiveProgram[selKey];
-//     }
-// }
-//
-// var plenterProgram = lib.thinning.plenter({
-//     id: 'MegaSTP_Plenter',
-//     schedule: { min: 1, opt: 10, max: 500 },
-//     interval:     () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.plenter.interval),
-//     plenterCurve: () => getFlag('abe_param_plenterCurve', MEGA_STP_DEFAULTS.plenter.plenterCurve)
-// });
-// for (var pKey in plenterProgram) {
-//     if (typeof plenterProgram[pKey] === 'object' && plenterProgram[pKey].type) {
-//          plenterProgram[pKey].onEvaluate = function() {
-//             return getFlag('abe_next_activity', 'none') === 'plenter';
-//          };
-//          MEGA_STP_ACTIVITIES['plenter_' + pKey] = plenterProgram[pKey];
-//     }
-// }
-//
-// var tendingProgram = lib.thinning.tending({
-//     id: 'MegaSTP_Tending',
-//     schedule: { min: 1, opt: 10, max: 500 },
-//     times:              () => getFlag('abe_param_times', MEGA_STP_DEFAULTS.tending.times),
-//     interval:           () => getFlag('abe_param_interval', MEGA_STP_DEFAULTS.tending.interval),
-//     intensity:          () => getFlag('abe_param_intensity', MEGA_STP_DEFAULTS.tending.intensity),
-//     speciesSelectivity: () => getFlag('abe_param_speciesSelectivity', MEGA_STP_DEFAULTS.tending.speciesSelectivity),
-//     sendSignal: 'Activity_Completed'
-// });
-// for (var tKey in tendingProgram) {
-//     if (typeof tendingProgram[tKey] === 'object' && tendingProgram[tKey].type) {
-//         tendingProgram[tKey].onEvaluate = function() {
-//             return getFlag('abe_next_activity', 'none') === 'tending';
-//         };
-//         MEGA_STP_ACTIVITIES['tending_' + tKey] = tendingProgram[tKey];
-//     }
-// }
-//
-// MEGA_STP_ACTIVITIES['planting'] = lib.planting.dynamic({
-//     id: 'MegaSTP_Planting',
-//     schedule: { min: 1, opt: 2, max: 5 },
-//     speciesSelectivity: () => getFlag('abe_param_speciesSelectivity', MEGA_STP_DEFAULTS.planting.speciesSelectivity),
-//     sendSignal: 'Activity_Completed'
-// });
-// MEGA_STP_ACTIVITIES['planting'].onEvaluate = function() {
-//     return getFlag('abe_next_activity', 'none') === 'planting';
-// };
-//
-// MEGA_STP_ACTIVITIES['salvage'] = lib.harvest.salvage({
-//     id: 'Universal_Salvage',
-//     clearAll: true,
-//     sendSignal: 'Activity_Completed'
-// });
-
-// --- III. FINAL STP ASSEMBLY ---
+// --- FINAL STP ASSEMBLY ---
 var MegaSTP = {
     U: [120, 150, 180],
     activities: MEGA_STP_ACTIVITIES,
