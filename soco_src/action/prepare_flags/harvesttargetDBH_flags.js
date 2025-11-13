655a0e55052a756d85ad377f8080015fb842b4ae
vullioud <colino.vullioud@gmail.com>
2025-11-13 08:20:21 +0100

    fix bug on arg passing

--- soco_src/action/prepare_flags/harvesttargetDBH_flags.js
+++ soco_src/action/prepare_flags/harvesttargetDBH_flags.js
@@ -1,17 +1,10 @@
-Action.prepare.targetDBH = function(params, stand_data_obj) {
+Action.prepare.targetDBH = function(params) {
     if (params && params.dbhListProfile) {
-        // The agent's plan provides the NAME of the profile to use.
-        var profile_name = params.dbhListProfile;
-        
-        // We get the actual DBH list object from the agent's config tables.
-        var agent = socoabe.institution.agents.find(a => a.id === stand_data_obj.agent_id);
-        if (agent && agent.targetDBH_profiles_table && agent.targetDBH_profiles_table[profile_name]) {
-            var dbhList = agent.targetDBH_profiles_table[profile_name];
-            stand.setFlag('abe_param_dbhList', dbhList);
-            console.log(`      -> Using dbhListProfile '${profile_name}'.`);
-        } else {
-            console.warn(`      -> WARN: dbhListProfile '${profile_name}' not found for agent ${agent.id}. Using empty list.`);
-            stand.setFlag('abe_param_dbhList', {});
-        }
+        // 'params.dbhListProfile' now correctly holds the full dbhList object.
+        stand.setFlag('abe_param_dbhList', params.dbhListProfile);
+        console.log(`      -> Setting 'abe_param_dbhList' flag from provided profile object.`);
+    } else {
+        console.warn(`      -> WARN: No 'dbhListProfile' object found in parameters for targetDBH. Setting empty flag.`);
+        stand.setFlag('abe_param_dbhList', {});
     }
 };
