c6fdd9943dab34024c3dc131e87eb355757267a7
vullioud <colino.vullioud@gmail.com>
2025-12-23 11:10:25 +0100

    update

--- soco_src/core/stand_data.js
+++ soco_src/core/stand_data.js
@@ -4,9 +4,9 @@ class stand_data {
     constructor(stand_id, agent) { // Changed signature to accept the agent object
         // --- I. IDENTIFIERS & FIXED TRAITS ---
         this.stand_id = stand_id;
-        this.agent_id = agent.id; // Get the ID from the agent object
-      //  this.agent = agent;       // Store the direct reference
-        this.preference_focus = "none"; // This will be set by the agent
+        this.agent_id = agent.id; 
+      //  this.agent = agent;     
+        this.preference_focus = "none"; 
         this.species_profile = "none";
 
         // --- II. PERCEPTION DATA ---
@@ -19,9 +19,6 @@ class stand_data {
             top_height: 0,
             species_count: 0,
             U: 0,
-            thinning_intensity: 'unknown',
-            time_since_last_activity_iLand: -1,
-            last_activity_name_iLand: 'none',
             year_of_observation: -1,
             needs_reassessment: false
         };
@@ -34,7 +31,8 @@ class stand_data {
             last_activity: 'none',
             last_activity_Year: -1, 
             time_since_last_activity: -1, 
-            last_satisfied_phase: 'none'
+            last_satisfied_phase: 'none', 
+            target_species: []
         };
 
         // --- III. AGENT'S PLAN ---
@@ -47,12 +45,12 @@ class stand_data {
             timeline: [],
             sequence_total_steps: 0,
             sequence_current_step: 0,
-            sequence_sub_activity: 'none' // <-- NEW PROPERTY ADDED HERE
+            sequence_sub_activity: 'none' 
         };
 
         // --- IV. MONITORING SNAPSHOT ---
         this.monitoringSnapshot = null;
-        this.is_monitoring_candidate = false; // Default to false
+        this.is_monitoring_candidate = false; 
         
         // --- V. DATA LOGS --- 
         // These were missing and are required by Monitoring.snapshot
